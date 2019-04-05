55c7601c71c74c7d8c29ef952ade6c1ad5e564ee
kloepric <ktloeprich@gmail.com>
2019-04-05 16:42:20 -0700

    returns the weight inputed after modifying for planets

--- planets.py
+++ planets.py
@@ -1,7 +1,8 @@
 def weight_on_planets():
    # write your code here
-   
-   
-   
+   weight = float(input("What do you weigh on earth? "))
+   mars_weight = weight*.38
+   jupiter_weight = weight*2.34
+   print("\n" + "On Mars you would weigh" , mars_weight , "pounds.\n" + "On Jupiter you would weigh" , jupiter_weight , "pounds.")
 if __name__ == '__main__':
    weight_on_planets()
